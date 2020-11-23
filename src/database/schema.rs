1384508037cb2ce1bc7615b91761d2055edd9125
SeraphyBR <luisjuniorbr@gmail.com>
2020-11-23 19:46:16 -0300

    Add initial generated diesel database scheme file

---
+++ src/database/schema.rs
@@ -0,0 +1,97 @@
+table! {
+    categories (id) {
+        id -> Uuid,
+        label -> Varchar,
+        tag_color -> Varchar,
+    }
+}
+
+table! {
+    clients (id) {
+        id -> Uuid,
+        name -> Varchar,
+        email -> Varchar,
+        phone_number -> Varchar,
+        address -> Varchar,
+    }
+}
+
+table! {
+    products (id) {
+        id -> Uuid,
+        name -> Varchar,
+        description -> Varchar,
+        store_name -> Nullable<Varchar>,
+        store_price -> Nullable<Numeric>,
+        price -> Numeric,
+        additional_charge -> Nullable<Numeric>,
+        color -> Varchar,
+        weight -> Numeric,
+        brand -> Varchar,
+        stock_amount -> Int4,
+    }
+}
+
+table! {
+    products_categories (id) {
+        id -> Uuid,
+        product_id -> Nullable<Uuid>,
+        category_id -> Nullable<Uuid>,
+        label -> Varchar,
+        tag_color -> Varchar,
+    }
+}
+
+table! {
+    requests (id) {
+        id -> Uuid,
+        user_id -> Nullable<Uuid>,
+        client_id -> Nullable<Uuid>,
+        sale_date -> Date,
+        delivery_date -> Date,
+        status -> Int4,
+        comments -> Nullable<Varchar>,
+        price -> Numeric,
+        discount -> Numeric,
+        freight -> Numeric,
+    }
+}
+
+table! {
+    requests_products (id) {
+        id -> Uuid,
+        request_id -> Nullable<Uuid>,
+        product_id -> Nullable<Uuid>,
+        amount -> Int4,
+        additional_costs -> Numeric,
+        discount -> Numeric,
+    }
+}
+
+table! {
+    users (id) {
+        id -> Uuid,
+        name -> Varchar,
+        email -> Varchar,
+        password -> Varchar,
+        share_photos -> Bool,
+        darkmode -> Bool,
+    }
+}
+
+joinable!(products_categories -> categories (category_id));
+joinable!(products_categories -> products (product_id));
+joinable!(requests -> clients (client_id));
+joinable!(requests -> users (user_id));
+joinable!(requests_products -> clients (product_id));
+joinable!(requests_products -> requests (request_id));
+
+allow_tables_to_appear_in_same_query!(
+    categories,
+    clients,
+    products,
+    products_categories,
+    requests,
+    requests_products,
+    users,
+);
