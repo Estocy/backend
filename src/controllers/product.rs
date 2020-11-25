d9650136d3380592db69d7ceeea1234c08f3552f
HenriqueFQuick <henrique.fquick@gmail.com>
2020-11-25 19:28:52 -0300

    Update index and show products

--- src/controllers/product.rs
+++ src/controllers/product.rs
@@ -2,8 +2,10 @@ use crate::database::connection::establish_connection;
 use crate::database::schema::products::dsl::products;
 use crate::database::schema::products_categories::dsl;
 use crate::database::schema::products_categories::dsl::products_categories;
-use crate::models::product::{Product, NewProduct, ProductReceiver};
-use crate::models::product_category::{NewProductCategory, ProductCategory};
+use crate::database::schema::categories::dsl::categories;
+use crate::models::category::{Category};
+use crate::models::product::{Product, ProductReceiver};
+use crate::models::product_category::{ProductCategory};
 use diesel::prelude::*;
 use rocket_contrib::json::Json;
 use uuid::Uuid;
@@ -28,25 +30,59 @@ pub fn create(product: Json<ProductReceiver>) -> Json<Product> {
 }
 
 #[get("/")]
-pub fn index() -> Json<Vec<Product>> {
+pub fn index() -> Json<Vec<(Product, Vec<Category>)>> {
     let connection = establish_connection();
     let results = products.load::<Product>(&connection)
         .expect("Error loading posts");
 
-    Json(results)
+    let mut list_products:Vec<(Product, Vec<Category>)> = Vec::new();
+    if results.len() != 0 {
+        for product in results{
+    
+            let results_product_category = products_categories.filter(dsl::product_id.eq(product.id))
+                .load::<ProductCategory>(&connection)
+                .expect("Error loading posts");
+
+            let mut list_categories= Vec::<Category>::new();
+            for product_category in results_product_category {
+                let results_product_category = categories.find(product_category.category_id)
+                    .load::<Category>(&connection)
+                    .expect("Error loading posts");
+                list_categories.push(results_product_category[0].clone());
+            }
+
+            list_products.push((product.clone(), list_categories.clone()))
+        }
+        
+    }
+    Json(list_products)
 }
 
 #[get("/<id>")]
-pub fn show(id: String) -> Option<Json<Product>> {
+pub fn show(id: String) -> Option<Json<(Product, Vec<Category>)>> {
     let connection = establish_connection();
     let results = products.find(Uuid::parse_str(id.as_str()).unwrap())
         .load::<Product>(&connection)
         .expect("Error loading posts");
-
+    
+   
     if results.len() == 0 {
         None
     } else {
-        Some(Json(results[0].clone()))
+   
+        let results_product_category = products_categories.filter(dsl::product_id.eq(Uuid::parse_str(id.as_str()).unwrap()))
+            .load::<ProductCategory>(&connection)
+            .expect("Error loading posts");
+
+        let mut list_categories= Vec::<Category>::new();
+        for product_category in results_product_category {
+            let results_product_category = categories.find(product_category.category_id)
+                .load::<Category>(&connection)
+                .expect("Error loading posts");
+            list_categories.push(results_product_category[0].clone());
+        }
+
+        Some(Json((results[0].clone(), list_categories.clone())))
     }
 }
 
