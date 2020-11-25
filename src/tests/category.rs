143e2acb4a0b6543e745c9c91bdf34d249a68a72
SeraphyBR <luisjuniorbr@gmail.com>
2020-11-25 07:04:40 -0300

    Update

--- src/tests/category.rs
+++ src/tests/category.rs
@@ -12,20 +12,24 @@ fn create() {
         label:"teste",
         tag_color: "#ffffff"
     };
+
     let response_category_expected = Category{
             id: Uuid::new_v4(),
             label: String::from("teste"),
             tag_color: String::from("#ffffff")
     };
+
     let mut response = client.post("/categories")
-    .header(ContentType::JSON)
-    .body(serde_json::to_string(&category).unwrap())
-    .dispatch();
+        .header(ContentType::JSON)
+        .body(serde_json::to_string(&category).unwrap())
+        .dispatch();
+
     assert_eq!(response.status(), Status::Ok);
+
     let response_category: Category = serde_json::from_str(response.body_string().unwrap().as_str()).unwrap();
+
     assert_eq!(response_category.label, response_category_expected.label);
     assert_eq!(response_category.tag_color, response_category_expected.tag_color);
-    //todo!();
 }
 
 #[test]
